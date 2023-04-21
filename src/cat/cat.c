1c3607a8d54a53105a62448a3f5cbf556fe93769
luzhixing12345 <luzhixing12345@163.com>
2023-04-22 02:59:38 +0800

    [feat] : finish main argument features

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -14,7 +14,10 @@
 // 全局变量
 static int show_end = 0;
 static int show_number = 0;
-static int line_number = 0;
+static int show_tab = 0;
+static int line_number = 1;
+static int nonblank = 0;
+static int squeeze = 0;
 char **files = NULL;
 
 
@@ -31,16 +34,54 @@ void XBOX_cat(const char *file_name) {
         perror(error_info);
         exit(1);
     }
-    char c;
-    while ((c = fgetc(fp)) != EOF) {
+    char c = fgetc(fp);
+    if (c == EOF) {
+        fclose(fp);
+        return;
+    }
+    if (nonblank && c == '\n') {
+        line_number--;
+    } else if (show_number) {
+        printf("%6d",line_number);
+    }
+    char old_c;
+    while (c != EOF) {
 
         // 一些cat的参数
-        if (show_end && c == '\n') {
-            printf("$");
+        if (show_tab && c == '\t') {
+            printf("^I");
         }
-
-        printf("%c",c);
+        old_c = c;
+        c = fgetc(fp);
+        if (squeeze && old_c == '\n' && c == '\n') {
+            char next_c = fgetc(fp);
+            if (next_c != '\n' && line_number) {
+                if (show_end) {
+                    printf("$");
+                }
+                printf("%c", old_c);
+            }
+            fseek(fp, -1, SEEK_CUR);
+        } else {
+            if (show_end && old_c == '\n') {
+                printf("$");
+            }
+            printf("%c", old_c);
+        }
+        if (nonblank && old_c == '\n' && c != EOF) {
+            if (c != old_c) {
+                line_number++;
+                printf("%6d\t",line_number);
+            }
+            continue;
+        }
+        if (show_number && old_c == '\n' && c != EOF) {
+            line_number++;
+            printf("%6d\t",line_number);
+        }
+        
     }
+    fclose(fp);
     return;
 }
 
@@ -52,13 +93,13 @@ int main(int argc, char const *argv[]) {
         XBOX_ARG_BOOLEAN(NULL, [-h][--help][help = "display this help and exit"]),
         XBOX_ARG_STR_GROUPS(&files, [name = FILE][help = "source"]),
         XBOX_ARG_BOOLEAN(NULL, [-A][--show-all][help = "equivalent to -vET"]),
-        XBOX_ARG_BOOLEAN(NULL, [-b][--number-nonblank][help = "number noempty output lines, overrides -n"]),
+        XBOX_ARG_BOOLEAN(&nonblank, [-b][--number-nonblank][help = "number noempty output lines, overrides -n"]),
         XBOX_ARG_BOOLEAN(NULL, [-e][name = e][help = "equivalent to -vE"]),
         XBOX_ARG_BOOLEAN(&show_end, [-E][--show-ends][help = "display $ at end of each line"]),
         XBOX_ARG_BOOLEAN(&show_number, [-n][--number][help = "number all output lines"]),
-        XBOX_ARG_BOOLEAN(NULL, [-s][--squeeze-blank][help = "suppress repeated empty output lines"]),
+        XBOX_ARG_BOOLEAN(&squeeze, [-s][--squeeze-blank][help = "suppress repeated empty output lines"]),
         XBOX_ARG_BOOLEAN(NULL, [-t][name = t][help = "equivalent to -vT"]),
-        XBOX_ARG_BOOLEAN(NULL, [-T][name = bigt][help = "display TAB character as ^I"]),
+        XBOX_ARG_BOOLEAN(&show_tab, [-T][name = bigt][help = "display TAB character as ^I"]),
         XBOX_ARG_BOOLEAN(NULL, [-u][name = u][help = (ignored)]),
         XBOX_ARG_BOOLEAN(NULL, [-v][--show-nonprinting][help = "use ^ and M- notation, except for LFD and TAB"]),
         XBOX_ARG_BOOLEAN(NULL, [--version][help = "output version information and exit"]),
--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 
 
-
+import os
+import subprocess
 
 def main():
     
-    ...
+    test_dir = 'testfiles'
+    target_dir = 'src'
+    exe_names = os.listdir(test_dir)
+    
+    for exe_name in exe_names:
+        
+        with open(os.path.join(test_dir,exe_name),'r',encoding='utf-8') as f:
+            shell_cmds = f.read().split('\n')
+        
+        # 默认程序
+        for shell_cmd in shell_cmds:
+            with open('output.txt','w') as f:
+                subprocess.call('ls',stdout=f)
 
 
 
